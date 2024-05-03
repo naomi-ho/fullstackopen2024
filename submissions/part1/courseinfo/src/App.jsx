c91bfa54654c88d9aad7da86d88ccad0ff2425d4
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-05-03 16:21:13 +1000

    Refactor code to include 3 components

--- submissions/part1/courseinfo/src/App.jsx
+++ submissions/part1/courseinfo/src/App.jsx
@@ -1,3 +1,32 @@
+// Header takes care of rendering the name of the course
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+// Content renders the parts and their number of exercises
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  );
+};
+
+// Total renders the total number of exercises
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercise}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +38,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total exercise={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
