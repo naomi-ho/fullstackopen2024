202fdb182e90e889cdebeccb78ce7c818821307c
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-05-08 00:54:35 +1000

    Add more statistics: total, average, percentage of positive feedback

--- submissions/part1/unicafe/src/App.jsx
+++ submissions/part1/unicafe/src/App.jsx
@@ -16,22 +16,62 @@ const Display = (props) => {
   );
 };
 
+const Total = (props) => {
+  return <div>all {props.total}</div>;
+};
+
+const Average = (props) => {
+  return <div>average {props.average}</div>;
+};
+
+const Percentage = (props) => {
+  return (
+    <div>
+      positive {props.percentage} {props.text}
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [count, setCount] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setCount(count + 1);
+    updateAverage(good + 1, bad, count + 1);
+    updatePercentage(good + 1, count + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setCount(count + 1);
+    updatePercentage(good, count + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setCount(count + 1);
+    updateAverage(good, bad + 1, count + 1);
+    updatePercentage(count + 1);
+    updatePercentage(good, count + 1);
+  };
+
+  const updateAverage = (good, bad, count) => {
+    if (count > 0) {
+      setAverage(good + (bad * -1) / count);
+    }
+  };
+
+  const updatePercentage = (good, count) => {
+    if (count > 0) {
+      setPercentage((good / count) * 100);
+    }
   };
 
   return (
@@ -44,6 +84,9 @@ const App = () => {
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Total total={good + neutral + bad} />
+      <Average average={average} />
+      <Percentage percentage={percentage} text="%" />
     </div>
   );
 };
